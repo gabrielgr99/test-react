2e3bf3968e948509243979b617084aaccb1d0d5a
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2025-10-16 23:19:10 -0300

    remove li from into CardMovie

--- app/views/home/components/card-movie/index.tsx
+++ app/views/home/components/card-movie/index.tsx
@@ -5,23 +5,21 @@ import { HeartIcon } from "lucide-react";
 
 export function CardMovie({ title, voteAverage, posterPath, onFavorite, isFavorite }: CardMovieProps) {
 	return (
-		<li>
-			<Card className="p-0 border-0 w-[200px] h-max gap-0">
-				<CardContent className="p-0 relative">
-					<HeartIcon
-						size={22}
-						fill={isFavorite ? "red" : "transparent"}
-						color="red"
-						className="bg-background p-1 hover:p-0.5 m-2 duration-300 rounded-full absolute cursor-pointer right-0"
-						onClick={onFavorite}
-					/>
-					<img alt="Movie poster" className="rounded-t-md" src={posterPath} />
-				</CardContent>
-				<CardHeader className="p-3">
-					<CardTitle className="text-sm font-medium overflow-hidden text-ellipsis text-nowrap" title={title}>{title}</CardTitle>
-					<VoteAverage voteAverage={voteAverage} />
-				</CardHeader>
-			</Card>
-		</li>
+		<Card className="p-0 border-0 w-[200px] h-max gap-0">
+			<CardContent className="p-0 relative">
+				<HeartIcon
+					size={22}
+					fill={isFavorite ? "red" : "transparent"}
+					color="red"
+					className="bg-background p-1 hover:p-0.5 m-2 duration-300 rounded-full absolute cursor-pointer right-0"
+					onClick={onFavorite}
+				/>
+				<img alt="Movie poster" className="rounded-t-md" src={posterPath} />
+			</CardContent>
+			<CardHeader className="p-3">
+				<CardTitle className="text-sm font-medium overflow-hidden text-ellipsis text-nowrap" title={title}>{title}</CardTitle>
+				<VoteAverage voteAverage={voteAverage} />
+			</CardHeader>
+		</Card>
 	);
 }
